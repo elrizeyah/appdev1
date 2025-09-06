e151946743d6e52b1bd3273e87c35e2cd6e0d8e2
elrizeyah <montes.ellissea.pasillos@gmail.com>
2025-09-06 10:04:49 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,14 @@
+//Using let
+let name = "Ellissea";
+let age = 24;
+age = 20;
+console.log("let - name:", name, "age:", age);
+
+//Using const
+const city = "Makati";
+// city = "Manila"; //This will throw an error because const can't be reassigned
+
+//Using var
+var country = "Philippines";
+var country = "Japan"; //Var can be redeclared
+console.log("var - country:", country);
