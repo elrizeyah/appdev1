161c6697e2560c7eae93da72b4d353629bb05056
elrizeyah <montes.ellissea.pasillos@gmail.com>
2025-09-06 09:28:50 +0800

    Add base syntax activity

---
+++ 11_javascript-refresher/01_base_syntax.js
@@ -0,0 +1,11 @@
+console.log("Hello JavaScript");
+
+// Variable names are case-sensitive, cannot be mixed.
+
+//Uppercase
+let myName = "Ellissea";
+//Lowercase
+let myname = "Montes";
+
+console.log("myName:", myName); // Ellissea
+console.log("myname:", myname); // Montes
