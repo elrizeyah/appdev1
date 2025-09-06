c01de1dfa2a03cda4ba484221210c4f03c543103
elrizeyah <montes.ellissea.pasillos@gmail.com>
2025-09-06 10:07:32 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.js
@@ -0,0 +1,10 @@
+// Default export: greeting function
+export default function greet() {
+  return "Hello from the module!";
+}
+
+// Named export: info object
+export const info = {
+  name: "Benedict",
+  age: 47
+};
