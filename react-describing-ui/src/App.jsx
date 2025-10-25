530edde25ed3a82224360aad2c41f935218608fd
elrizeyah <montes.ellissea.pasillos@gmail.com>
2025-10-25 10:51:29 +0800

    Done with how to write your first React component

---
+++ react-describing-ui/src/App.jsx
@@ -0,0 +1,17 @@
+// How to write your first React component
+
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg"
+  alt="Katherine Johnson" />;
+}
+
+export default function Gallery() {
+  return (
+    <>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      {/* <Profile> */}
+    </>
+  );
+}
