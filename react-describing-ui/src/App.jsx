556899706dc4a6fe95b1dcc81c31ffe0f58f52fe
elrizeyah <montes.ellissea.pasillos@gmail.com>
2025-10-25 10:58:18 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -18,15 +18,35 @@
 
 // When and how to create multi-component files
 
-import Profile from './components/Profile.jsx';
+// import Profile from './components/Profile.jsx';
 
-export default function Gallery() {
+// export default function Gallery() {
+//   return (
+//     <>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </>
+//   );
+// }
+
+// How to add markup to JavaScript with JSX
+
+export default function TodoList() {
   return (
     <>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
     </>
   );
 }
