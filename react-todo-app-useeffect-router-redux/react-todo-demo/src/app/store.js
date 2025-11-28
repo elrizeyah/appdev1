35afb9f586fd8f73e192aea8fab1d152b3c41d8e
Ellissea Montes <montes.ellissea.pasillos@gmail.com>
2025-11-28 19:27:29 +0800

    Done with set up React Router section

--- react-todo-app-useeffect-router-redux/react-todo-demo/src/app/store.js
+++ react-todo-app-useeffect-router-redux/react-todo-demo/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todosReducer from "../features/todos/todosSlice";
+// import todosReducer from "../features/todos/todosSlice";
 
 export const store = configureStore({
   reducer: {
-    todos: todosReducer,
+    // todos: todosReducer,
+    todos:[]
   },
 });
